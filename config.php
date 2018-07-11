90d0f9fe1f32ad537e09073084501660da3313cc
Henry Harris <henrythomasharris@gmail.com>
2018-07-11 19:49:05 +0200

    Added config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        'database' => [
+            'host' => '127.0.0.1',
+            'name' => 'database',
+            'user' => 'root',
+            'pass' => 'password',
+        ],
+    ];
+?>
