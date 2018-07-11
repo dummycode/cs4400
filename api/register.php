9f60d9909f56dd3da48a11cb5702c7601187677c
Henry Harris <henrythomasharris@gmail.com>
2018-07-11 20:09:39 +0200

    Added create tables for now

--- api/register.php
+++ api/register.php
@@ -1,29 +1,32 @@
 <?php
-  $server = "127.0.0.1";
-  $username = "root";
-  $password = "password";
-  $db = "bcn_museums";
+    $config = require('../config.php');
+    $database = $config['database'];
+    // Database configurations
+    $host = $database['host'];
+    $user = $database['user'];
+    $pass = $database['pass'];
+    $name = $database['name'];
 
-  // Create connection
-  $conn = mysqli_connect($server, $username, $password, $db);
-  if (!$conn) {
-    echo "Failed to connect to database";
-  }
+    // Create connection
+    $conn = mysqli_connect($host, $user, $pass, $name);
+    if (!$conn) {
+        echo "Failed to connect to database";
+    }
 
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $card_number = $_POST['card_number'];
-  $exp_month = $_POST['exp_month'];
-  $exp_year = $_POST['exp_year'];
-  $security_number = $_POST['security_number'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $card_number = $_POST['card_number'];
+    $exp_month = $_POST['exp_month'];
+    $exp_year = $_POST['exp_year'];
+    $security_number = $_POST['security_number'];
 
-  $sql = "INSERT INTO User (id, email, password) VALUES (null, '" . $_POST['email'] . "', '" . $_POST['password'] . "')";
-  if (mysqli_query($conn, $sql)) {
-    echo "Good";
-    setcookie('token', 'test', time() + (30), "/");
-    header("Location: ../index.php");
-    die();
-  } else {
-    echo "Failed query<br>" . mysqli_error($conn);
-  }
+    $sql = "INSERT INTO User (id, email, password) VALUES (null, '" . $_POST['email'] . "', '" . $_POST['password'] . "')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Good";
+        setcookie('token', 'test', time() + (30), "/");
+        header("Location: ../index.php");
+        die();
+    } else {
+        echo "Failed query<br>" . mysqli_error($conn);
+    }
 ?>
