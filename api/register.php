e24848e615990d7ff09c6ff8e225c5268b97475f
Henry Harris <henrythomasharris@gmail.com>
2018-07-11 20:23:31 +0200

    Shitty ass working registration

--- api/register.php
+++ api/register.php
@@ -13,20 +13,36 @@
         echo "Failed to connect to database";
     }
 
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $card_number = $_POST['card_number'];
-    $exp_month = $_POST['exp_month'];
-    $exp_year = $_POST['exp_year'];
-    $security_number = $_POST['security_number'];
-
-    $sql = "INSERT INTO User (id, email, password) VALUES (null, '" . $_POST['email'] . "', '" . $_POST['password'] . "')";
-    if (mysqli_query($conn, $sql)) {
-        echo "Good";
-        setcookie('token', 'test', time() + (30), "/");
-        header("Location: ../index.php");
-        die();
+    if (count($_POST) != 6) {
+        echo "Incorrect arguments";
     } else {
-        echo "Failed query<br>" . mysqli_error($conn);
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $card_number = $_POST['card_number'];
+        $exp_month = $_POST['exp_month'];
+        $exp_year = $_POST['exp_year'];
+        $security_number = $_POST['security_number'];
+
+        $sql = "
+            INSERT INTO Visitor (id, email, password, card_number, exp_month, exp_year, security_number)
+            VALUES (
+                null,
+                '" . $_POST['email'] . "',
+                '" . $_POST['password'] . "',
+                '" . $_POST['card_number'] . "',
+                '" . $_POST['exp_month'] . "',
+                '" . $_POST['exp_year'] . "',
+                '" . $_POST['security_number'] . "'
+            )
+        ";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "Good";
+            setcookie('token', 'test', time() + (30), "/");
+            header("Location: ../index.php");
+            die();
+        } else {
+            echo "Failed query<br>" . mysqli_error($conn);
+        }
     }
 ?>
