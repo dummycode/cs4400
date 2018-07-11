5e046eb0aa25df35101577d59649e9e9497dfb81
Henry Harris <henrythomasharris@gmail.com>
2018-07-11 20:11:04 +0200

    Fix indent

--- register.php
+++ register.php
@@ -1,14 +1,14 @@
 <html>
-  <body>
-    <form name="register" method="post" action="api/register.php">
-      Email: <input type="text" name="email"><br>
-      Password: <input type="password" name="password"><br>
-      Confirm Password: <input type="password" name="password2"><br>
-      Credit Card Number: <input type="text" name="card_number"><br>
-      Credit Card Exp. Month: <input type="text" name="exp_month"><br>
-      Credit Card Exp. Year: <input type="text" name="exp_year"><br>
-      Credit Card Security Code: <input type="text" name="security_number" placeholder="123"><br>
-      <input type="submit" name="submit" value="Submit"><br>
-    </form>
-  </body>
+    <body>
+        <form name="register" method="post" action="api/register.php">
+            Email: <input type="text" name="email"><br>
+            Password: <input type="password" name="password"><br>
+            Confirm Password: <input type="password" name="password2"><br>
+            Credit Card Number: <input type="text" name="card_number"><br>
+            Credit Card Exp. Month: <input type="text" name="exp_month"><br>
+            Credit Card Exp. Year: <input type="text" name="exp_year"><br>
+            Credit Card Security Code: <input type="text" name="security_number" placeholder="123"><br>
+            <input type="submit" name="submit" value="Submit"><br>
+        </form>
+    </body>
 </html>
