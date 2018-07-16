9e176908334ed301a2e6feddf2133645e0673e02
Henry Harris <henrythomasharris@gmail.com>
2018-07-16 12:10:56 +0200

    Confirm delete

--- manage.php
+++ manage.php
@@ -1,4 +1,20 @@
 <html>
+    <head>
+        <script>
+            function confirmDelete() {
+                document.getElementById("confirmation").hidden = "";
+                document.getElementById("noButton").hidden = "";
+                document.getElementById("yesButton").type = "submit";
+                document.getElementById("delete").hidden = "hidden";
+            }
+            function cancelDelete() {
+                document.getElementById("confirmation").hidden = "hidden";
+                document.getElementById("noButton").hidden = "hidden";
+                document.getElementById("yesButton").type = "hidden";
+                document.getElementById("delete").hidden = "";
+            }
+        </script>
+    </head>
     <body>
         <form method="post" action="api/users.php">
             <input type="hidden" name="action" value="logout">
@@ -9,7 +25,10 @@
         </form>
         <form method="post" action="api/users.php">
             <input type="hidden" name="action" value="delete">
-            <input type="submit" value="Delete Account"/>
+            <button id="delete" type="button" onclick="confirmDelete()">Delete Account</button>
+            <span id="confirmation" hidden="hidden">Are you sure?</span>
+            <button id="noButton" type="button" value="No" hidden="hidden" onclick="cancelDelete()">No</button>
+            <input id="yesButton" value="Yes" type="hidden">
         </form>
         <form action="index.php">
             <input type="submit" value="Back"/>
